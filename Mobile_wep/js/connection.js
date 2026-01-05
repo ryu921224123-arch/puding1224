a3e1e8a08690bbc63a84b1ce2cb9604ef28a1b4f
ryu921224123-arch <ryu921224123@gmail.com>
2026-01-05 20:41:39 +0900

    Delete Mobile_wep directory

--- Mobile_wep/js/connection.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const retryBtn = document.getElementById("retryBtn");
-
-  if (retryBtn) {
-    retryBtn.addEventListener("click", () => {
-      location.href = "index.html";
-    });
-  }
-});
